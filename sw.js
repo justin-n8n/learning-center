951656706250b423f73b2c45a72be6de19773046
justin-n8n <s8825523@gmail.com>
2026-07-06 22:10:07 +0800

    v6: 繼續上課區＋課程狀態/進度條＋週統計圖與連續天數＋排序＋深色模式＋骨架屏

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'learning-center-v5';
+const CACHE = 'learning-center-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
